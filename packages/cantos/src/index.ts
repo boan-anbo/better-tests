a847f521d15eefaa33ab98bf4e8f4a45027d26f0
Bo <gazagoal@gmail.com>
2023-11-27 01:46:56 -0500

    refactor: export domain entity types

--- packages/cantos/src/index.ts
+++ packages/cantos/src/index.ts
@@ -1,6 +1,6 @@
 import {UserStory} from "@src/stories/stories.ts";
 import {StoryScript} from "@src/stories/story-types.ts";
-import {DomainDef, DomainObjectsDef, UserDomain} from "@src/stories/interfaces.ts";
+import {DomainDef, DomainObjectsDef, UserDomain, DomainEntity, DomainEvent, DomainCommand} from "@src/stories/interfaces.ts";
 // import and export entrance as Acts or A
 import * as entrance from "./entrance.ts";
 import {CommonScences, CommonTest, customizeCommonTests} from "./commons.ts";
@@ -14,7 +14,7 @@ export {entrance as Cantos, entrance as C};
 
 
 // Export types for story
-export type {StoryScript, UserStory, DomainObjectsDef, DomainDef, UserDomain}
+export type {StoryScript, UserStory, DomainObjectsDef, DomainDef, UserDomain, DomainEntity, DomainEvent, DomainCommand}
 
 // export Common Test helpers
 export {CommonScences as CS, CommonTest as CT, CommonScences, CommonTest, customizeCommonTests}
